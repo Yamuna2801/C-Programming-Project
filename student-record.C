cdf374dade6588be12791e132120ac8feda3cd3b
Yamuna2801 <yamunaravi2801@gmail.com>
2025-07-12 01:05:14 +0530

    Add the student record file

--- student-record.C
+++ student-record.C
@@ -1,57 +1,43 @@
-/*#include <stdio.h>
-
-int main() {
-  FILE *fp;
-  char c[100];
-  //clrscr();
-
-  // Create a file on your computer (filename.txt)
-  fp = fopen("for-loop.txt", "r");
-
-  fgets(c, 6, fp);
-  printf("%s", c);
-  // Close the file
-  //fclose(fptr);
-
-  getchar();
-  return 0;
-}*/
-
-
-
-
-
-
 #include<stdio.h>
 #include<string.h>
 
-struct student {
-       char name[50];
-       int roll;
-       float marks;
+struct record {
+     int roll;
+     char name[50];
+     int marks;
 };
 
+
 int main() {
-    struct student students[100];
+    struct record students[50];
     int i, n;
-    clrscr();
+    //clrscr();
 
     printf("How many students? ");
     scanf("%d", &n);
 
-    for(i=0; i<n; i++) {
-      printf("Enter the details for student %d:\n", i+1);
-      printf("Name: ");
-      scanf("%s", &students[i].name);
-      printf("Roll Number: ");
+    for(i = 0; i < n; i++) {
+
+      printf("Enter the details for students %d:\n", i+1);
+      printf("Enter Roll Number: ");
       scanf("%d", &students[i].roll);
-      printf("Marks: ");
-      scanf("%f", &students[i].marks);
+
+      printf("Enter Name: ");
+      scanf("%s", &students[i].name);
+
+      printf("Enter Marks: ");
+      scanf("%d", &students[i].marks);
+
+    }
+
+    printf("\n--- Student Records ---\n");
+    for(i = 0; i < n; i++) {
+      printf("Student %d:\n", i+1);
+      printf("Roll Number: %d\n", students[i].roll);
+      printf("Name: %s\n", students[i].name);
+      printf("Marks: %d\n\n", students[i].marks);
     }
 
     getchar();
     return 0;
 }
-
-
-
