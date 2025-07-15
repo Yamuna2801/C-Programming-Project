cd3b00efd0e1fc3b01ee474c8f210553fbf0b051
Yamuna2801 <yamunaravi2801@gmail.com>
2025-07-15 15:20:07 +0530

    Add the files

--- library-management.c
+++ library-management.c
@@ -27,16 +27,16 @@ int main() {
       };
 
     int i;
-    char bookName1[10];
-    char bookName2[10];
-    char bookName3[10];
+    //char bookName1[10];
+    //char bookName2[10];
+    //char bookName3[10];
     clrscr();
 
     printf("------ Library Management ------\n");
     printf("Admin Name: %s\t", frontDesk.adminName);
     printf("Admin Id: %d\n\n", frontDesk.adminId);
 
-    printf("------ User Information ------\n");
+      printf("------ User Information ------\n");
       printf("Enter User Name: ");
       scanf("%s", &frontDesk.userName);
       printf("Enter User ID: ");
@@ -45,10 +45,11 @@ int main() {
       scanf("%d", &frontDesk.numberOfBooks);
 	if(frontDesk.numberOfBooks!=0 && frontDesk.numberOfBooks <=3) {
 	  for(i=0; i<frontDesk.numberOfBooks; i++) {
-	   printf("Enter Book Name - %d:", i+1);
-	   scanf("%s", &bookName1);
-	   scanf("%s", &bookName2);
-	   scanf("%s", &bookName3);
+	    printf("Enter Book Name - %d:", i+1);
+	    scanf("%s", &frontDesk.requestedBooks);
+	    //scanf("%s", &bookName1);
+	    //scanf("%s", &bookName2);
+	    //scanf("%s", &bookName3);
 	  }
 	}else{
 	    printf("Select valid book count \n\n");
